525ddd613179611ce7ecb52fe3777e998ffc047c
dante <heywheredante@gmail.com>
2022-07-07 10:59:27 +0900

    시큐리티로그인완료

---
+++ src/main/java/com/pg/security1/config/auth/PrincipalDetails.java
@@ -0,0 +1,77 @@
+package com.pg.security1.config.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.pg.security1.model.User;
+
+// security가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
+// 로그인이 진행이 완료가되면 시큐리티 session을 만들어준다. (Security ContextHolder)
+// 오브젝트 => Authentication 타입 객체
+// Authentication 안에 User정보가 있어야됨.
+// User오브젝트타입 => UserDetails 타입 객체
+
+// Security Session => Authentication => UserDetails(PrincipalDetails)
+
+public class PrincipalDetails implements UserDetails{
+
+	private User user;
+	
+	public PrincipalDetails(User user) {
+		this.user = user;
+	}
+	
+	// 해당 User의 권한을 리턴하는곳
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Collection<GrantedAuthority> collect = new ArrayList<>();
+		collect.add(new GrantedAuthority() {
+			
+			@Override
+			public String getAuthority() {
+				return user.getRole();
+			}
+		});
+		
+		return collect;
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		
+		//사이트에서 1년동안 로그인을 안하면 휴면 계정으로 하기로 함.
+		//현재시간-로긴시간 => 1년을 초과하면 return false;
+		
+		return true;
+	}
+
+}
